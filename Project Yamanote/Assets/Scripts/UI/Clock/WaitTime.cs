4346a9d3384cba5d16152fe9b2e52cdd4444db15
Domenic Pittari <dom.pittari@gmail.com>
2021-02-28 14:14:55 +1100

    Clock finally working

--- Project Yamanote/Assets/Scripts/UI/Clock/WaitTime.cs
+++ Project Yamanote/Assets/Scripts/UI/Clock/WaitTime.cs
@@ -19,33 +19,32 @@ namespace ProjectYamanote.UI
         private void Start()
         {
             _waitTime.alpha = 0;
-            _waitTimeProgressBar.color.a.Equals(0);
+            _waitTimeProgressBar.DOFade(0, 0.1f);
         }
 
         private void Update()
         {
-            currentVal = (int)_timeSlider.value;           
+            currentVal = (int)((int)GameClock.dateTime.TimeOfDay.TotalMinutes + _timeSlider.value);           
+
             _waitTimeProgressBar.fillAmount = Normalise();
         }
 
         public void ChangeWaitTime()
         {
             _waitTime.DOFade(1, 1);
+            _waitTimeProgressBar.DOFade(1, 1);
 
             time = TimeSpan.FromMinutes(_timeSlider.value);
-            _waitTime.text = (DateTime.MinValue + time).ToString("HH:mm") + " hours";
-            
-            _waitTimeProgressBar.fillAmount = (float)time.TotalMinutes;
-            _waitTimeProgressBar.DOFade(1, 1);
+            _waitTime.text = (DateTime.MinValue + time).ToString("HH:mm") + " hours";   
         }
 
         public void ClickWait()
         {
-            _waitTime.DOFade(0, 1);
-            
+            _waitTime.DOFade(0, 1);           
             _waitTimeProgressBar.DOFade(0, 1);
 
-            GameClock.dateTime = GameClock.dateTime.Add(time);
+            GameClock.dateTime = GameClock.dateTime.AddMinutes(time.TotalMinutes);
+            _timeSlider.value = 0;
         }
 
         private float Normalise()
